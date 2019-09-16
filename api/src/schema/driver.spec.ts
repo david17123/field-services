27f03f1c4e6795610d27499092ea5f0332f4f12b
David <david.andika@uptickhq.com>
2019-09-16 10:11:36 +1000

    Added test for driver end point

--- api/src/schema/driver.spec.ts
+++ api/src/schema/driver.spec.ts
@@ -1,1 +1,53 @@
-// TODO Write some tests for driver end points
+import {
+  getConnection,
+  getDb,
+  setConnectionString,
+} from '../db/connection'
+import { activeSessions, activityLogs, drivers } from '../db/fixtures'
+import { resolvers } from './driver'
+
+describe('test driver end points', () => {
+  beforeAll(() => {
+    setConnectionString(global.__MONGO_URI__, global.__MONGO_DB_NAME__)
+  })
+
+  afterAll(async () => {
+    const connection = await getConnection()
+    await connection.close()
+  })
+
+  beforeEach(async () => {
+    const db = await getDb()
+    const driverCollection = db.collection('driver')
+    await driverCollection.insertMany(drivers)
+    const activeSessionCollection = db.collection('activeSession')
+    await activeSessionCollection.insertMany(activeSessions)
+    const activityLogCollection = db.collection('activityLog')
+    await activityLogCollection.insertMany(activityLogs)
+  })
+
+  afterEach(async () => {
+    const db = await getDb()
+    const driverCollection = db.collection('driver')
+    driverCollection.deleteMany({})
+    const activeSessionCollection = db.collection('activeSession')
+    activeSessionCollection.deleteMany({})
+    const activityLogCollection = db.collection('activityLog')
+    activityLogCollection.deleteMany({})
+  })
+
+  it('login user and start a new active session', async () => {
+    const loginResult = await resolvers.DriverMutation.login(null, {
+      device: {
+        os: 'ios',
+        osVersion: '10.0.0',
+      },
+      driver: {
+        password: drivers[0].password,
+        username: drivers[0].username,
+      },
+    })
+
+    expect(loginResult.driverId.toHexString()).toEqual(drivers[0]._id.toHexString())
+  })
+})
